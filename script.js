86b22c6250ff557ed04d5a6aa5e1e8a5808c3276
yanghuixi1 <yanghui.xi1@gmail.com>
2022-04-20 23:18:19 -0400

    Add and style buttons

--- script.js
+++ script.js
@@ -1,43 +1,80 @@
-let wins = 0;
-let losses = 0;
-let ties = 0;
-let computerChoices = ["R", "P", "S"];
+const selectionButtons = document.querySelectorAll("[data-selection]");
+const SELECTIONS = [
+  {
+    name: "rock",
+    emoji: "✊",
+    beats: "scissors",
+  },
+  {
+    name: "rock",
+    emoji: "✋",
+    beats: "scissors",
+  },
+  {
+    name: "rock",
+    emoji: "✌️",
+    beats: "scissors",
+  },
+];
 
-let playGame = function () {
-  let userChoose = prompt("Please enter R, P or S to play");
-  if (!userChoose) {
-    return;
-  }
+function makeSelection(selection) {
+  console.log(selection);
+}
 
-  userChoose = userChoose.toUpperCase();
-  let index = Math.floor(Math.random() * computerChoices.length);
-  let computerChoose = computerChoices[index];
-  alert(`Computer chose ${computerChoose}`);
+selectionButtons.forEach((selectionButton) => {
+  selectionButton.addEventListener("click", (e) => {
+    const selectionName = selectionButton.dataset.selection; // Attributes can be set and read by the camelCase name/key as an object property of the dataset: element.dataset.keyname
+    const selection = SELECTIONS.find(
+      (selection) => selection.name === selectionName
+    );
+    makeSelection(selection);
+  });
+});
 
-  if (userChoose === computerChoose) {
-    alert("It's a tie");
-    ties++;
-  } else if (
-    (userChoose === "S" && computerChoose === "P") ||
-    (userChoose === "R" && computerChoose === "S") ||
-    (userChoose === "P" && computerChoose === "R")
-  ) {
-    alert("You won!");
-    wins++;
-  } else {
-    (userChoose === "P" && computerChoose === "S") ||
-      (userChoose === "S" && computerChoose === "P") ||
-      (userChoose === "R" && computerChoose === "P");
-    alert("Computer won!");
-    losses++;
-  }
-  let print = alert(`wins = ${wins} \n losses = ${losses} \n ties = ${ties}`);
+function randomSelection() {
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+  return SELECTIONS.randomIndex;
+}
+// let wins = 0;
+// let losses = 0;
+// let ties = 0;
+// let computerChoices = ["R", "P", "S"];
 
-  //   let playAgain = function () {
-  let isPlay = confirm("Do you want to play again?");
-  if (isPlay) {
-    playGame();
-  }
-};
+// let playGame = function () {
+//   let userChoose = prompt("Please enter R, P or S to play");
+//   if (!userChoose) {
+//     return;
+//   }
 
-playGame();
+//   userChoose = userChoose.toUpperCase();
+//   let index = Math.floor(Math.random() * computerChoices.length);
+//   let computerChoose = computerChoices[index];
+//   alert(`Computer chose ${computerChoose}`);
+
+//   if (userChoose === computerChoose) {
+//     alert("It's a tie");
+//     ties++;
+//   } else if (
+//     (userChoose === "S" && computerChoose === "P") ||
+//     (userChoose === "R" && computerChoose === "S") ||
+//     (userChoose === "P" && computerChoose === "R")
+//   ) {
+//     alert("You won!");
+//     wins++;
+//   } else {
+//     (userChoose === "P" && computerChoose === "S") ||
+//       (userChoose === "S" && computerChoose === "P") ||
+//       (userChoose === "R" && computerChoose === "P");
+//     alert("Computer won!");
+//     losses++;
+//   }
+//   let print = alert(`wins = ${wins} \n losses = ${losses} \n ties = ${ties}`);
+
+//   //   let playAgain = function () {
+//   let isPlay = confirm("Do you want to play again?");
+//   if (isPlay) {
+//     playGame();
+//   }
+// };
+
+// playGame();
